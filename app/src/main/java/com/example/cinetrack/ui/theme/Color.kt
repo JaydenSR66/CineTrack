b197adf522efba23b876d1e9f647696ff27e5a76
Jayden R <jaydensouterrobertson@gmail.com>
2026-04-28 22:12:12 +0100

    Added Light and Dark Themes - JR

--- app/src/main/java/com/example/cinetrack/ui/theme/Color.kt
+++ app/src/main/java/com/example/cinetrack/ui/theme/Color.kt
@@ -2,10 +2,20 @@ package com.example.cinetrack.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// Light Theme Colours
+val LightPrimary = Color(0xFFE50914)
+val LightSecondary = Color(0xFFFFB800)
+val LightBackground = Color(0xFFF5F5F5)
+val LightSurface = Color(0xFFFFFFFF)
+val LightOnPrimary = Color(0xFFFFFFFF)
+val LightOnBackground = Color(0xFF1B1B1B)
+val LightOnSurface = Color(0xFF1B1B1B)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// Dark Theme Colours
+val DarkPrimary = Color(0xFFE50914)
+val DarkSecondary = Color(0xFFFFB800)
+val DarkBackground = Color(0xFF121212)
+val DarkSurface= Color(0xFF1E1E1E)
+val DarkOnPrimary = Color(0xFFFFFFFF)
+val DarkOnBackground = Color(0xFFFFFFFF)
+val DarkOnSurface = Color(0xFFE0E0E0)
