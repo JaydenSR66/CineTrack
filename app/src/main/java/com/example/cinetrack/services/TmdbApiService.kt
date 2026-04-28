bcd9cfca555b065e67dd724916afee6a5d2a9ec9
Jayden R <jaydensouterrobertson@gmail.com>
2026-04-28 17:53:21 +0100

    Implemented TmdbApiService.kt and confirmed API connection working - JR

---
+++ app/src/main/java/com/example/cinetrack/services/TmdbApiService.kt
@@ -0,0 +1,38 @@
+package com.example.cinetrack.services
+
+import com.example.cinetrack.models.MovieResponse
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+/*
+Handles all communication with the TMDB API
+ */
+object TmdbApiService {
+    // Base URL for TMDB API Requests
+    private const val base_url = "https://api.themoviedb.org/3/"
+
+    // Builds the HTTP client with the base URL and Gson converter
+    private val Http_Client = Retrofit.Builder()
+        .baseUrl(base_url)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    // Creates an instance of ApiInterface
+    val api: ApiInterface by lazy {
+        Http_Client.create(ApiInterface::class.java)
+    }
+
+    /*
+    Get request to TMDB for returning list of popular movies
+     */
+    interface ApiInterface {
+        @GET("movie/popular")
+
+        suspend fun getPopularMovies(
+            @Query("api_key") apiKey: String,
+            @Query("page") page: Int = 1
+        ): MovieResponse
+    }
+}
